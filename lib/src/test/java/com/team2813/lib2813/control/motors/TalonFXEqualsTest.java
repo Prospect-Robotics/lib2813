410e8305294fb3d56373596ce19ff58bde0ab85d
cuttestkittensrule <mangoiscute95@gmail.com>
2024-02-03 15:04:45 -0800

    fixed warnings

--- lib/src/test/java/com/team2813/lib2813/control/motors/TalonFXEqualsTest.java
+++ lib/src/test/java/com/team2813/lib2813/control/motors/TalonFXEqualsTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import com.ctre.phoenix6.hardware.TalonFX;
 import com.team2813.lib2813.control.InvertType;
 
 public class TalonFXEqualsTest {
