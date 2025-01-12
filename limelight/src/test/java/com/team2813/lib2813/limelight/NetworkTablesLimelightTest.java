8cdfdc15717aac0085360fae1e38240b3a3019f3
Kevin Cooney <kcooney@gmail.com>
2025-01-11 19:53:54 -0800

    Extract LimelightTestCase

--- limelight/src/test/java/com/team2813/lib2813/limelight/NetworkTablesLimelightTest.java
+++ limelight/src/test/java/com/team2813/lib2813/limelight/NetworkTablesLimelightTest.java
@@ -21,74 +21,25 @@ import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
-public class NetworkTablesLimelightTest {
+public class NetworkTablesLimelightTest extends LimelightTestCase {
   private static final String TABLE_NAME = "limelight";
 
   @After
   public void resetNetworkTables() {
     getJsonNTEntry().setString("");
   }
-  
-  @Test
-  public void emptyValues() {
-    NetworkTablesLimelight limelight = new NetworkTablesLimelight(TABLE_NAME);
-    
-    OptionalDouble latency = limelight.getCaptureLatency();
-    
-    assertFalse("NetworkTables should be empty", latency.isPresent());
+
+  @Override
+  protected Limelight createLimelight() {
+    return new NetworkTablesLimelight(TABLE_NAME);
   }
 
-  @Test
-  public void absentTest1() throws Exception {
-    getJsonNTEntry().setString(readJSON("AbsentTest1.json"));
-    NetworkTablesLimelight limelight = new NetworkTablesLimelight(TABLE_NAME);
-    
-    LocationalData locationalData = limelight.getLocationalData();
-    assertFalse(locationalData.getBotpose().isPresent());
-    OptionalDouble actualCaptureLatency = locationalData.getCaptureLatency();
-    assertAlmostEqual(37.40, actualCaptureLatency, 0.005);
-    OptionalDouble actualTargetingLatency = locationalData.getTargetingLatency();
-    assertAlmostEqual(38.95, actualTargetingLatency, 0.005);
-  }
-  
-  @Test
-  public void presentTest() throws Exception {
-    getJsonNTEntry().setString(readJSON("PresentTest1.json"));
-    NetworkTablesLimelight limelight = new NetworkTablesLimelight(TABLE_NAME);
-    
-    LocationalData locationalData = limelight.getLocationalData();
-    Optional<Pose3d> actualPose = locationalData.getBotpose();
-    assertTrue(actualPose.isPresent());
-    OptionalDouble actualCaptureLatency = locationalData.getCaptureLatency();
-    assertAlmostEqual(37.40, actualCaptureLatency, 0.005);
-    OptionalDouble actualTargetingLatency = locationalData.getTargetingLatency();
-    assertAlmostEqual(66.61, actualTargetingLatency, 0.005);
-    Rotation3d rotation = new Rotation3d(Math.toRadians(6.817779398227925), Math.toRadians(-25.663211825857257), Math.toRadians(-173.13543891950323));
-    Pose3d expectedPose = new Pose3d(7.3505718968031255, 0.7083545864687876, 0.9059300968047116, rotation);
-    assertEquals(expectedPose, actualPose.orElse(null));
-    assertAlmostEqual(3664865.25, limelight.getTimestamp(), 0.005);
-  }
-
-  private static void assertAlmostEqual(double expected, OptionalDouble actual, double delta) {
-    actual.ifPresentOrElse(
-            d -> assertEquals(expected, d, delta),
-            () -> assertEquals(OptionalDouble.of(expected), actual));
+  @Override
+  protected void setJson(JSONObject json) {
+    getJsonNTEntry().setString(json.getJSONObject("Results").toString());
   }
   
   private static NetworkTableEntry getJsonNTEntry() {
     return LimelightHelpers.getLimelightNTTableEntry(TABLE_NAME, "json");
-  }
-
-  String readJSON(String fileName) throws IOException {
-    try (InputStream is = getClass().getResourceAsStream(fileName)) {
-      if (is == null) {
-        throw new FileNotFoundException(fileName);
-      }
-      try (InputStreamReader isr = new InputStreamReader(is);
-           BufferedReader reader = new BufferedReader(isr)) {
-        JSONObject json = new JSONObject(reader.lines().collect(Collectors.joining(System.lineSeparator())));
-        return json.getJSONObject("Results").toString();
-      }
-    }
   }
 }
--- limelight/src/test/java/com/team2813/lib2813/limelight/RestLimelightTest.java
+++ limelight/src/test/java/com/team2813/lib2813/limelight/RestLimelightTest.java
@@ -28,7 +28,7 @@ import org.junit.Test;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 
-public class RestLimelightTest {
+public class RestLimelightTest extends LimelightTestCase {
 	@Rule
 	public final FakeLimelight fakeLimelight = new FakeLimelight();
 
@@ -39,108 +39,13 @@ public class RestLimelightTest {
 
 	@Test
 	public void equality() {
-		Limelight a = Limelight.getDefaultLimelight();
-		Limelight b = Limelight.getDefaultLimelight();
+		Limelight a = RestLimelight.getDefaultLimelight();
+		Limelight b = RestLimelight.getDefaultLimelight();
 		assertEquals("Default limelight call returned different values", a, b);
 		Limelight c = RestLimelight.getLimelight(RestLimelight.DEFAULT_ADDRESS);
 		assertEquals(
 				"Default limelights not equal to limelights named \"limelight\" (default)",
 				a, c);
-	}
-
-	@Test
-	public void emptyValues() {
-		RestLimelight limelight = createLimelight();
-		assertFalse("JSON should be empty", limelight.getCaptureLatency().isPresent());
-	}
-	
-	JSONObject readJSON(String fileName) throws IOException {
-		try (InputStream is = getClass().getResourceAsStream(fileName)) {
-			if (is == null) {
-				throw new FileNotFoundException(fileName);
-			}
-			try (InputStreamReader isr = new InputStreamReader(is);
-					BufferedReader reader = new BufferedReader(isr)) {
-				return new JSONObject(reader.lines().collect(Collectors.joining(System.lineSeparator())));
-			}
-		}
-	}
-
-	@Test
-	public void absentTest1() throws Exception {
-		JSONObject obj = readJSON("AbsentTest1.json");
-		fakeLimelight.setResultsResponse(obj);
-		Limelight limelight = createLimelight();
-		LocationalData locationalData = limelight.getLocationalData();
-		assertFalse(locationalData.getBotpose().isPresent());
-		OptionalDouble actualCaptureLatency = locationalData.getCaptureLatency();
-		assertAlmostEqual(37.40, actualCaptureLatency, 0.005);
-		OptionalDouble actualTargetingLatency = locationalData.getTargetingLatency();
-		assertAlmostEqual(38.95, actualTargetingLatency, 0.005);
-	}
-
-	@Test
-	public void absentTest2() throws Exception {
-		JSONObject obj = readJSON("AbsentTest2.json");
-		fakeLimelight.setResultsResponse(obj);
-		Limelight limelight = createLimelight();
-		LocationalData locationalData = limelight.getLocationalData();
-		assertFalse(locationalData.getBotpose().isPresent());
-		OptionalDouble actualCaptureLatency = locationalData.getCaptureLatency();
-		assertAlmostEqual(37.40, actualCaptureLatency, 0.005);
-		OptionalDouble actualTargetingLatency = locationalData.getTargetingLatency();
-		assertAlmostEqual(54.64, actualTargetingLatency, 0.005);
-	}
-
-	private static void assertAlmostEqual(double expected, OptionalDouble actual, double delta) {
-		actual.ifPresentOrElse(
-						d -> assertEquals(expected, d, delta),
-						() -> assertEquals(OptionalDouble.of(expected), actual));
-	}
-
-	@Test
-	public void presentTest1() throws Exception {
-		JSONObject obj = readJSON("PresentTest1.json");
-		fakeLimelight.setResultsResponse(obj);
-		Limelight limelight = createLimelight();
-		assertTrue(limelight.hasTarget());
-		LocationalData locationalData = limelight.getLocationalData();
-		Optional<Pose3d> actualPose = locationalData.getBotpose();
-		assertTrue(actualPose.isPresent());
-		OptionalDouble actualCaptureLatency = locationalData.getCaptureLatency();
-		assertAlmostEqual(37.40, actualCaptureLatency, 0.005);
-		OptionalDouble actualTargetingLatency = locationalData.getTargetingLatency();
-		assertAlmostEqual(66.61, actualTargetingLatency, 0.005);
-		Rotation3d rotation = new Rotation3d(Math.toRadians(6.817779398227925), Math.toRadians(-25.663211825857257), Math.toRadians(-173.13543891950323));
-		Pose3d expectedPose = new Pose3d(7.3505718968031255, 0.7083545864687876, 0.9059300968047116, rotation);
-		assertEquals(expectedPose, actualPose.orElse(null));
-		assertAlmostEqual(3664865.25, limelight.getTimestamp(), 0.005);
-	}
-
-	@Test
-	public void presentTest2() throws Exception {
-		JSONObject obj = readJSON("PresentTest2.json");
-		fakeLimelight.setResultsResponse(obj);
-		Limelight limelight = createLimelight();
-		assertTrue(limelight.hasTarget());
-		LocationalData locationalData = limelight.getLocationalData();
-		Optional<Pose3d> actualPose = locationalData.getBotpose();
-		assertTrue(actualPose.isPresent());
-		OptionalDouble actualCaptureLatency = locationalData.getCaptureLatency();
-		assertAlmostEqual(37.40, actualCaptureLatency, 0.005);
-		OptionalDouble actualTargetingLatency = locationalData.getTargetingLatency();
-		assertAlmostEqual(59.20, actualTargetingLatency, 0.005);
-		Rotation3d rotation = new Rotation3d(Math.toRadians(-5.176760596073282), Math.toRadians(-24.321885146945643), Math.toRadians(-164.63614172918574));
-		Pose3d expectedPose = new Pose3d(7.46915459715645, 0.8066093109325925, 1.0062389106931178, rotation);
-		assertEquals(expectedPose, actualPose.orElse(null));
-		assertTrue(limelight.getTimestamp().isPresent());
-		assertEquals(941200.41, limelight.getTimestamp().getAsDouble(), 0.005);
-	}
-
-	private RestLimelight createLimelight() {
-		RestLimelight limelight = new RestLimelight("localhost");
-		limelight.runThread();
-        return limelight;
 	}
 
 	@Test
@@ -150,5 +55,17 @@ public class RestLimelightTest {
 		HttpClient client = HttpClient.newBuilder().executor(Executors.newSingleThreadExecutor()).build();
 		HttpResponse<String> response = client.send(req, BodyHandlers.ofString());
 		assertEquals("{\"v\":1}", response.body());
+	}
+
+	@Override
+	protected Limelight createLimelight() {
+		RestLimelight limelight = new RestLimelight("localhost");
+		limelight.runThread();
+		return limelight;
+	}
+
+	@Override
+	protected void setJson(JSONObject json) {
+		fakeLimelight.setResultsResponse(json);
 	}
 }
