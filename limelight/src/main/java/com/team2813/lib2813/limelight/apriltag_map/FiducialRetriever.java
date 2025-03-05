d8e7940f2f3f33c7e2097f230f44d43e958642c1
Kevin Cooney <kcooney@gmail.com>
2025-03-04 23:24:07 -0800

    Pass Charset to InputStreamReader

--- limelight/src/main/java/com/team2813/lib2813/limelight/apriltag_map/FiducialRetriever.java
+++ limelight/src/main/java/com/team2813/lib2813/limelight/apriltag_map/FiducialRetriever.java
@@ -5,12 +5,15 @@ import edu.wpi.first.wpilibj.Filesystem;
 
 import java.awt.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class FiducialRetriever {
   private static final Gson gson = new Gson();
   private final Fiducial[] fiducials;
   public FiducialRetriever(InputStream stream) {
-    FieldMap map = gson.fromJson(new InputStreamReader(stream), FieldMap.class);
+    FieldMap map = gson.fromJson(new InputStreamReader(stream, UTF_8), FieldMap.class);
     fiducials = map.fiducials;
   }
   
