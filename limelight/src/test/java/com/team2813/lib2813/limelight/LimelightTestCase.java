1780f741390d55583eb0571bdaf4dbd7b1686a15
Kevin Cooney <kcooney@users.noreply.github.com>
2025-03-17 15:40:45 -0700

    Update limelight tests to use Truth (#32)

--- limelight/src/test/java/com/team2813/lib2813/limelight/LimelightTestCase.java
+++ limelight/src/test/java/com/team2813/lib2813/limelight/LimelightTestCase.java
@@ -2,6 +2,7 @@ package com.team2813.lib2813.limelight;
 
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import org.json.JSONObject;
 import org.junit.Test;
 
@@ -12,14 +13,20 @@ import java.util.OptionalDouble;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.*;
+import static com.google.common.truth.Truth.assertAbout;
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+import static com.team2813.lib2813.limelight.Pose2dSubject.pose2ds;
+import static org.junit.Assert.assertEquals;
+import static com.team2813.lib2813.limelight.Pose3dSubject.pose3ds;
 
 abstract class LimelightTestCase {
 
 	@Test
 	public final void emptyValues() {
 		Limelight limelight = createLimelight();
-		assertFalse("JSON should be empty", limelight.getCaptureLatency().isPresent());
+		assertWithMessage("JSON should be empty")
+				.that(limelight.getCaptureLatency()).isEmpty();
 	}
 
 	@Test
@@ -28,15 +35,15 @@ abstract class LimelightTestCase {
 		setJson(obj);
 		Limelight limelight = createLimelight();
 		LocationalData locationalData = limelight.getLocationalData();
-		assertFalse(locationalData.getBotpose().isPresent());
+		assertThat(locationalData.getBotpose()).isEmpty();
 		OptionalDouble actualCaptureLatency = locationalData.getCaptureLatency();
 		double expectedCaptureLatencyMs = 37.40;
 		assertAlmostEqual(expectedCaptureLatencyMs, actualCaptureLatency, 0.005);
 		OptionalDouble actualTargetingLatency = locationalData.getTargetingLatency();
 		double expectedTargetingLatencyMs = 38.95;
 		assertAlmostEqual(expectedTargetingLatencyMs, actualTargetingLatency, 0.005);
-		assertFalse(locationalData.getBotPoseEstimateBlue().isPresent());
-		assertFalse(locationalData.getBotPoseEstimateRed().isPresent());
+		assertThat(locationalData.getBotPoseEstimateBlue()).isEmpty();
+		assertThat(locationalData.getBotPoseEstimateRed()).isEmpty();
 	}
 
 	@Test
@@ -45,15 +52,15 @@ abstract class LimelightTestCase {
 		setJson(obj);
 		Limelight limelight = createLimelight();
 		LocationalData locationalData = limelight.getLocationalData();
-		assertFalse(locationalData.getBotpose().isPresent());
+		assertThat(locationalData.getBotpose()).isEmpty();
 		OptionalDouble actualCaptureLatency = locationalData.getCaptureLatency();
 		double expectedCaptureLatencyMs = 37.40;
 		assertAlmostEqual(expectedCaptureLatencyMs, actualCaptureLatency, 0.005);
 		OptionalDouble actualTargetingLatency = locationalData.getTargetingLatency();
 		double expectedTargetingLatencyMs = 54.64;
 		assertAlmostEqual(expectedTargetingLatencyMs, actualTargetingLatency, 0.005);
-		assertFalse(locationalData.getBotPoseEstimateBlue().isPresent());
-		assertFalse(locationalData.getBotPoseEstimateRed().isPresent());
+		assertThat(locationalData.getBotPoseEstimateBlue()).isEmpty();
+		assertThat(locationalData.getBotPoseEstimateRed()).isEmpty();
 	}
 
 	@Test
@@ -61,39 +68,38 @@ abstract class LimelightTestCase {
 		JSONObject obj = readJSON("PresentTest1.json");
 		setJson(obj);
 		Limelight limelight = createLimelight();
-		assertTrue(limelight.hasTarget());
+		assertHasTarget(limelight);
 		LocationalData locationalData = limelight.getLocationalData();
-		Optional<Pose3d> actualPose = locationalData.getBotpose();
-		assertTrue(actualPose.isPresent());
 		OptionalDouble actualCaptureLatency = locationalData.getCaptureLatency();
 		double expectedCaptureLatencyMs = 37.40;
 		assertAlmostEqual(expectedCaptureLatencyMs, actualCaptureLatency, 0.005);
 		OptionalDouble actualTargetingLatency = locationalData.getTargetingLatency();
 		double expectedTargetingLatencyMs = 66.61;
 		assertAlmostEqual(expectedTargetingLatencyMs, actualTargetingLatency, 0.005);
+		Optional<Pose3d> actualPose = locationalData.getBotpose();
+		assertThat(actualPose).isPresent();
+		Rotation3d rotation = new Rotation3d(Math.toRadians(6.82), Math.toRadians(-25.66), Math.toRadians(-173.14));
+		Pose3d expectedPose = new Pose3d(7.35, 0.71, 0.91, rotation);
+		assertAbout(pose3ds()).that(actualPose.get()).isWithin( 0.005).of(expectedPose);
 
-		Rotation3d rotation = new Rotation3d(Math.toRadians(6.817779398227925), Math.toRadians(-25.663211825857257), Math.toRadians(-173.13543891950323));
-		Pose3d expectedPose = new Pose3d(7.3505718968031255, 0.7083545864687876, 0.9059300968047116, rotation);
-		assertEquals(expectedPose, actualPose.orElse(null));
+		assertThat(locationalData.getBotPoseEstimate()).isPresent();
+		var poseEstimate = locationalData.getBotPoseEstimate().get();
+		assertThat(poseEstimate.timestampSeconds()).isGreaterThan(0.0);
+    	expectedPose = new Pose3d(7.35, 0.71, 0.0, new Rotation3d(0.0, 0.0, rotation.getZ()));
+		assertAbout(pose2ds()).that(poseEstimate.pose()).isWithin( 0.005).of(expectedPose);
 
-		var poseEstimate = locationalData.getBotPoseEstimate();
-		assertTrue(poseEstimate.isPresent());
-		assertTrue(poseEstimate.get().timestampSeconds() > 0);
-		expectedPose = new Pose3d(7.3505718968031255, 0.7083545864687876, 0.9059300968047116, rotation);
-		assertEquals(poseEstimate.get().pose(), expectedPose.toPose2d());
+		assertThat(locationalData.getBotPoseEstimateBlue()).isPresent();
+		var blueEstimate = locationalData.getBotPoseEstimateBlue().get();
+		assertThat(blueEstimate.timestampSeconds()).isGreaterThan(0.0);
+		expectedPose = new Pose3d(15.62, 4.52, 0.0, new Rotation3d(0.0, 0.0, rotation.getZ()));
+		assertAbout(pose2ds()).that(blueEstimate.pose()).isWithin( 0.005).of(expectedPose);
 
-		var blueEstimate = locationalData.getBotPoseEstimateBlue();
-		assertTrue(blueEstimate.isPresent());
-		assertTrue(blueEstimate.get().timestampSeconds() > 0);
-		expectedPose = new Pose3d(15.621446896803125, 4.515204586468787, 0.9059300968047116, rotation);
-		assertEquals(blueEstimate.get().pose(), expectedPose.toPose2d());
-
-		var redEstimate = locationalData.getBotPoseEstimateRed();
-		assertTrue(redEstimate.isPresent());
-		assertEquals(blueEstimate.get().timestampSeconds(), redEstimate.get().timestampSeconds(), 0.005);
-		rotation = new Rotation3d(Math.toRadians(6.817779398227925), Math.toRadians(-25.663211825857257), Math.toRadians(6.8644090410054215));
-		expectedPose = new Pose3d(0.9203012235144277, 3.0985149189332175, 0.9059300968047116, rotation);
-		assertEquals(redEstimate.get().pose(), expectedPose.toPose2d());
+		assertThat(locationalData.getBotPoseEstimateRed()).isPresent();
+		var redEstimate = locationalData.getBotPoseEstimateRed().get();
+		assertThat(redEstimate.timestampSeconds()).isWithin(0.005).of(blueEstimate.timestampSeconds());
+		rotation = new Rotation3d(Math.toRadians(6.82), Math.toRadians(-25.66), Math.toRadians(6.86));
+		expectedPose = new Pose3d(0.92, 3.10, 0, new Rotation3d(0.0, 0.0, rotation.getZ()));
+		assertAbout(pose2ds()).that(redEstimate.pose()).isWithin( 0.005).of(expectedPose);
 	}
 
 	@Test
@@ -101,10 +107,8 @@ abstract class LimelightTestCase {
 		JSONObject obj = readJSON("PresentTest2.json");
 		setJson(obj);
 		Limelight limelight = createLimelight();
-		assertTrue(limelight.hasTarget());
+		assertHasTarget(limelight);
 		LocationalData locationalData = limelight.getLocationalData();
-		Optional<Pose3d> actualPose = locationalData.getBotpose();
-		assertTrue(actualPose.isPresent());
 		OptionalDouble actualCaptureLatency = locationalData.getCaptureLatency();
 		double expectedCaptureLatencyMs = 37.40;
 		assertAlmostEqual(expectedCaptureLatencyMs, actualCaptureLatency, 0.005);
@@ -114,19 +118,19 @@ abstract class LimelightTestCase {
 
 		Rotation3d rotation = new Rotation3d(Math.toRadians(-5.176760596073282), Math.toRadians(-24.321885146945643), Math.toRadians(-164.63614172918574));
 		Pose3d expectedPose = new Pose3d(7.46915459715645, 0.8066093109325925, 1.0062389106931178, rotation);
-		assertEquals(expectedPose, actualPose.orElse(null));
+		assertThat(locationalData.getBotpose()).hasValue(expectedPose);
 
 		var poseEstimate = locationalData.getBotPoseEstimate();
-		assertTrue(poseEstimate.isPresent());
+		assertThat(poseEstimate).isPresent();
 
 		var blueEstimate = locationalData.getBotPoseEstimateBlue();
-		assertTrue(blueEstimate.isPresent());
-		assertTrue(blueEstimate.get().timestampSeconds() > 0);
+		assertThat(blueEstimate).isPresent();
+		assertThat(blueEstimate.get().timestampSeconds()).isGreaterThan(0.0);
 		expectedPose = new Pose3d(15.74002959715645, 4.8134593109325925, 1.00623891069311787116, rotation);
 		assertEquals(blueEstimate.get().pose(), expectedPose.toPose2d());
 
 		var redEstimate = locationalData.getBotPoseEstimateRed();
-		assertTrue(redEstimate.isPresent());
+		assertThat(redEstimate).isPresent();
 		rotation = new Rotation3d(Math.toRadians(-5.176760596073282), Math.toRadians(-24.321885146945643), Math.toRadians(15.363706231322912));
 		expectedPose = new Pose3d(0.8017182624333765, 3.200260509139247, 1.0062389106931178, rotation);
 		assertEquals(redEstimate.get().pose(), expectedPose.toPose2d());
@@ -138,20 +142,15 @@ abstract class LimelightTestCase {
 		JSONObject obj = readJSON("BotposeBlueRedTest.json");
 		setJson(obj);
 		Limelight limelight = createLimelight();
-    assertTrue(limelight.hasTarget());
+		assertHasTarget(limelight);
 		
 		LocationalData locationalData = limelight.getLocationalData();
 		Optional<Pose3d> botposeBlue = locationalData.getBotposeBlue();
-		assertTrue(botposeBlue.isPresent());
+		assertThat(botposeBlue).isPresent();
 		Pose3d actualPose = botposeBlue.get();
-		
-		Rotation3d expectedRotation = new Rotation3d(0, 0, Math.toRadians(-123.48705171149771));
-		Pose3d expectedPose = new Pose3d(4.715193569870748, 5.203922172240444, 0, expectedRotation);
-		double poseDiff = actualPose.getTranslation().getDistance(expectedPose.getTranslation());
-		assertAlmostEqual(0, OptionalDouble.of(poseDiff), 0.05);
-		Rotation3d rotationDiff = expectedRotation.minus(actualPose.getRotation());
-		double angleDiff = rotationDiff.getAngle();
-		assertAlmostEqual(0, OptionalDouble.of(angleDiff), Math.PI / 12.0);
+		Rotation3d expectedRotation = new Rotation3d(0, 0, Math.toRadians(-123.49));
+		Pose3d expectedPose = new Pose3d(4.72, 5.20, 0, expectedRotation);
+		assertAbout(pose3ds()).that(actualPose).isWithin( 0.005).of(expectedPose);
 	}
 	
 	@Test
@@ -159,29 +158,24 @@ abstract class LimelightTestCase {
 		JSONObject obj = readJSON("BotposeBlueRedTest.json");
 		setJson(obj);
 		Limelight limelight = createLimelight();
-		assertTrue(limelight.hasTarget());
+		assertHasTarget(limelight);
 		
 		LocationalData locationalData = limelight.getLocationalData();
 		Optional<Pose3d> botposeBlue = locationalData.getBotposeRed();
-		assertTrue(botposeBlue.isPresent());
+		assertThat(botposeBlue).isPresent();
 		Pose3d actualPose = botposeBlue.get();
 		
-		Rotation3d expectedRotation = new Rotation3d(0, 0, Math.toRadians(56.51279624901089));
-		Pose3d expectedPose = new Pose3d(11.825855015367468, 3.0070683933666404, 0, expectedRotation);
-		double poseDiff = actualPose.getTranslation().getDistance(expectedPose.getTranslation());
-		assertAlmostEqual(0, OptionalDouble.of(poseDiff), 0.05);
-		Rotation3d rotationDiff = expectedRotation.minus(actualPose.getRotation());
-		double angleDiff = rotationDiff.getAngle();
-		assertAlmostEqual(0, OptionalDouble.of(angleDiff), Math.PI / 12.0);
+		Rotation3d expectedRotation = new Rotation3d(0, 0, Math.toRadians(56.51));
+		Pose3d expectedPose = new Pose3d(11.83, 3.01, 0, expectedRotation);
+		assertAbout(pose3ds()).that(actualPose).isWithin( 0.005).of(expectedPose);
 	}
 
 	@Test
-	public final void visibleTagTest() throws Exception {
+	public final void visibleTagsTest() throws Exception {
 		JSONObject obj = readJSON("BotposeBlueRedTest.json");
 		setJson(obj);
 		Limelight limelight = createLimelight();
-		assertTrue(limelight.hasTarget());
-
+		assertHasTarget(limelight);
 		Set<Integer> tags = limelight.getLocationalData().getVisibleTags();
 		assertEquals(Set.of(20), tags);
 	}
@@ -198,8 +192,13 @@ abstract class LimelightTestCase {
 		}
 
 		Set<Integer> visibleTags = limelight.getLocationalData().getVisibleTags();
-		List<Pose3d> apriltags = limelight.getLocatedAprilTags(visibleTags);
-		assertEquals(1, apriltags.size());
+		List<Pose3d> aprilTags = limelight.getLocatedAprilTags(visibleTags);
+		assertEquals(1, aprilTags.size());
+		Pose3d pose = aprilTags.get(0);
+		assertAbout(pose3ds())
+				.that(pose).translation()
+				.isWithin( 0.005)
+				.of(new Translation3d(-3.87, 0.72, 0.31));
 	}
 
 	protected abstract Limelight createLimelight();
@@ -216,6 +215,11 @@ abstract class LimelightTestCase {
 				return new JSONObject(reader.lines().collect(Collectors.joining(System.lineSeparator())));
 			}
 		}
+	}
+
+	private void assertHasTarget(Limelight limelight) {
+		assertWithMessage("Should have target").that(limelight.hasTarget()).isTrue();
+		assertWithMessage("Should have target").that(limelight.getLocationalData().hasTarget()).isTrue();
 	}
 
 	protected static void assertAlmostEqual(double expected, OptionalDouble actual, double delta) {
---
+++ limelight/src/test/java/com/team2813/lib2813/limelight/Pose3dSubject.java
@@ -0,0 +1,208 @@
+package com.team2813.lib2813.limelight;
+
+import com.google.common.truth.DoubleSubject;
+import com.google.common.truth.FailureMetadata;
+import com.google.common.truth.Subject;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
+import javax.annotation.Nullable;
+
+import static com.google.common.truth.Fact.simpleFact;
+import static com.google.common.truth.Truth.assertAbout;
+
+/**
+ * Truth Subject for making assertions about {@link Pose3d} values.
+ *
+ * <p>See <a href="https://truth.dev/extension">Writing your own custom subject</a>
+ * to learn about creating custom Truth subjects.
+ */
+class Pose3dSubject extends Subject {
+
+    // User-defined entry point
+    public static Pose3dSubject assertThat(@Nullable Pose3d pose) {
+        return assertAbout(pose3ds()).that(pose);
+    }
+
+    // Static method for getting the subject factory (for use with assertAbout())
+    public static Subject.Factory<Pose3dSubject, Pose3d> pose3ds() {
+        return Pose3dSubject::new;
+    }
+
+    private final Pose3d actual;
+
+    protected Pose3dSubject(FailureMetadata failureMetadata, @Nullable Pose3d subject) {
+        super(failureMetadata, subject);
+        this.actual = subject;
+    }
+
+    // User-defined test assertion SPI below this point
+
+    public TolerantComparison<Pose3d> isWithin(double tolerance) {
+        return new TolerantComparison<Pose3d>() {
+          @Override
+          public void of(Pose3d expected) {
+            translation().isWithin(tolerance).of(expected.getTranslation());
+            rotation().isWithin(tolerance).of(expected.getRotation());
+          }
+        };
+    }
+
+    // Chained subjects methods below this point
+
+    public Translation3dSubject translation() {
+        return check("getTranslation()").about(Translation3dSubject.translation3ds()).that(nonNullActualPose().getTranslation());
+    }
+
+    public Rotation3dSubject rotation() {
+        return check("getRotation()").about(Rotation3dSubject.rotation3ds()).that(nonNullActualPose().getRotation());
+    }
+
+    // Helper methods below this point
+
+    private Pose3d nonNullActualPose() {
+        if (actual == null) {
+            failWithActual(simpleFact("expected a non-null Pose3d"));
+        }
+        return actual;
+    }
+
+    /** Truth Subject for making assertions about {@link Translation3d} values. */
+    static class Translation3dSubject extends Subject {
+
+        // User-defined entry point
+        public static Translation3dSubject assertThat(@Nullable Translation3d translation) {
+            return assertAbout(translation3ds()).that(translation);
+        }
+
+        // Static method for getting the subject factory (for use with assertAbout())
+        public static Factory<Translation3dSubject, Translation3d> translation3ds() {
+            return Translation3dSubject::new;
+        }
+
+        private final Translation3d actual;
+
+        private Translation3dSubject(FailureMetadata failureMetadata, @Nullable Translation3d subject) {
+            super(failureMetadata, subject);
+            this.actual = subject;
+        }
+
+        // User-defined test assertion SPI below this point
+
+        public TolerantComparison<Translation3d> isWithin(double tolerance) {
+            return new TolerantComparison<Translation3d>() {
+                @Override
+                public void of(Translation3d expected) {
+                    x().isWithin(tolerance).of(expected.getX());
+                    y().isWithin(tolerance).of(expected.getY());
+                    z().isWithin(tolerance).of(expected.getZ());
+                }
+            };
+        }
+
+        public void isZero() {
+            if (!Translation3d.kZero.equals(actual)) {
+                failWithActual(simpleFact("expected to be zero"));
+            }
+        }
+
+        // Chained subjects methods below this point
+
+        public DoubleSubject x() {
+            return check("getX()").that(nonNullActual().getX());
+        }
+
+        public DoubleSubject y() {
+            return check("getY()").that(nonNullActual().getY());
+        }
+
+        public DoubleSubject z() {
+            return check("getZ()").that(nonNullActual().getZ());
+        }
+
+        // Helper methods below this point
+
+        private Translation3d nonNullActual() {
+            if (actual == null) {
+                failWithActual(simpleFact("expected a non-null Translation3d"));
+            }
+            return actual;
+        }
+    }
+
+    /** Truth Subject for making assertions about {@link Rotation3d} values. */
+    static class Rotation3dSubject extends Subject {
+        // User-defined entry point
+        public static Rotation3dSubject assertThat(@Nullable Rotation3d rotation) {
+            return assertAbout(rotation3ds()).that(rotation);
+        }
+
+        // Static method for getting the subject factory (for use with assertAbout())
+        public static Factory<Rotation3dSubject, Rotation3d> rotation3ds() {
+            return Rotation3dSubject::new;
+        }
+
+        private final Rotation3d actual;
+
+        private Rotation3dSubject(FailureMetadata failureMetadata, @Nullable Rotation3d subject) {
+            super(failureMetadata, subject);
+            this.actual = subject;
+        }
+
+        // User-defined test assertion SPI below this point
+
+        public TolerantComparison<Rotation3d> isWithin(double tolerance) {
+            return new TolerantComparison<Rotation3d>() {
+                @Override
+                public void of(Rotation3d expected) {
+                    x().isWithin(tolerance).of(expected.getX());
+                    y().isWithin(tolerance).of(expected.getY());
+                    z().isWithin(tolerance).of(expected.getZ());
+                }
+            };
+        }
+
+        public void isZero() {
+            if (!Rotation3d.kZero.equals(actual)) {
+                failWithActual(simpleFact("expected to be zero"));
+            }
+        }
+
+        // Chained subjects methods below this point
+
+        public DoubleSubject x() {
+            return check("getX()").that(nonNullActual().getX());
+        }
+
+        public DoubleSubject y() {
+            return check("getY()").that(nonNullActual().getY());
+        }
+
+        public DoubleSubject z() {
+            return check("getZ()").that(nonNullActual().getZ());
+        }
+
+        // Helper methods below this point
+
+        private Rotation3d nonNullActual() {
+            if (actual == null) {
+                failWithActual(simpleFact("expected a non-null Rotation3d"));
+            }
+            return actual;
+        }
+    }
+
+    /**
+     * A partially specified check about an approximate relationship to a {@code double} subject using
+     * a tolerance.
+     */
+    interface TolerantComparison<T> {
+
+        /**
+         * Fails if the subject was expected to be within the tolerance of the given value but was not.
+         * The subject and tolerance are specified earlier in the fluent call chain.
+         */
+        void of(T expected);
+    }
+}
