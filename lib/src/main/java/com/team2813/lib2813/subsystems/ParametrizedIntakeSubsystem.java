eedc812be76c7b475c8e9d77509c700a736758ae
atlanticbomber <atlanticbomber@gmail.com>
2025-09-10 15:06:41 -0700

    Attempted to add the ParametrizedIntakeSubsystem.java from Robot 2025 to this repository so we can use it in future bots. Currently does not compile

---
+++ lib/src/main/java/com/team2813/lib2813/subsystems/ParametrizedIntakeSubsystem.java
@@ -0,0 +1,106 @@
+package com.team2813.lib2813.subsystems;
+import com.google.auto.value.AutoBuilder;
+import com.team2813.lib2813.control.ControlMode;
+import com.team2813.lib2813.control.PIDMotor;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+abstract class ParameterizedIntakeSubsystem extends SubsystemBase implements AutoCloseable {
+    private final PIDMotor intakeMotor;
+    private final Params params;
+
+    public record Params(ControlMode controlMode, double intakeDemand, double outtakeDemand) {
+
+        public static Params.Builder builder() {
+            return new AutoBuilder_ParameterizedIntakeSubsystem_Params_Builder()
+                    .setControlMode(ControlMode.VOLTAGE);
+        }
+
+        @AutoBuilder
+        public interface Builder {
+            Builder setControlMode(ControlMode controlMode);
+
+            Builder setIntakeDemand(double demand);
+
+            Builder setOuttakeDemand(double demand);
+
+            Params build();
+        }
+
+        public Params {
+            if (controlMode == null) {
+                throw new IllegalArgumentException("controlMode cannot be null");
+            }
+            if (isEssentiallyZero(intakeDemand)) {
+                throw new IllegalArgumentException("intakeDemand cannot be zero");
+            }
+            if (isEssentiallyZero(outtakeDemand)) {
+                throw new IllegalArgumentException("outtakeDemand cannot be zero");
+            }
+            if (Math.signum(intakeDemand) == Math.signum(outtakeDemand)) {
+                throw new IllegalArgumentException(
+                        "intakeDemand should be the opposite sign of outtakeDemand");
+            }
+        }
+    }
+
+    protected ParameterizedIntakeSubsystem(PIDMotor intakeMotor, Params params) {
+        this.intakeMotor = intakeMotor;
+        this.params = params;
+    }
+
+    public final Command intakeItemCommand() {
+        return new InstantCommand(this::intakeGamePiece, this);
+    }
+
+    public final Command outtakeItemCommand() {
+        return new InstantCommand(this::outtakeGamePiece, this);
+    }
+
+    public final Command stopMotorCommand() {
+        return new InstantCommand(this::stopMotor, this);
+    }
+
+    /** Makes intake wheels spin in the intake direction. */
+    protected final void intakeGamePiece() {
+        // FIXME: Maybe add a check that the wheels are not stalled.
+        setMotorDemand(params.intakeDemand);
+    }
+
+    /** Makes intake wheels spin in the outtake direction. */
+    protected final void outtakeGamePiece() {
+        setMotorDemand(params.outtakeDemand);
+    }
+
+    /**
+     * Runs the motor with the provided demand value.
+     *
+     * @param demand Demand of the motor. Meaning depends on the {@code ControlMode}.
+     */
+    protected final void setMotorDemand(double demand) {
+        intakeMotor.set(params.controlMode, demand);
+    }
+
+    /**
+     * Returns a command that runs the motor with the provided demand value.
+     *
+     * @param demand Demand of the motor. Meaning depends on the {@code ControlMode}.
+     */
+    protected final Command setMotorDemandCommand(double demand) {
+        return new InstantCommand(() -> setMotorDemand(demand), this);
+    }
+
+    /** Stops the motor. */
+    public final void stopMotor() {
+        setMotorDemand(0.0);
+    }
+
+    @Override
+    public void close() {}
+
+    private static boolean isEssentiallyZero(double value) {
+        return Math.abs(value) < 0.001;
+    }
+}
+
