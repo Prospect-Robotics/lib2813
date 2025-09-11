5a4319528255f2253efe4cd95a1fe6590f150e46
atlanticbomber <atlanticbomber@gmail.com>
2025-09-11 11:03:05 -0700

    Fixed File naming error and made ParameterizedIntakeSubsystem public

--- lib/src/main/java/com/team2813/lib2813/subsystems/ParameterizedIntakeSubsystem.java
+++ lib/src/main/java/com/team2813/lib2813/subsystems/ParameterizedIntakeSubsystem.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-abstract class ParameterizedIntakeSubsystem extends SubsystemBase implements AutoCloseable {
+public abstract class ParameterizedIntakeSubsystem extends SubsystemBase implements AutoCloseable {
   private final PIDMotor intakeMotor;
   private final Params params;
 
