546f5b0e134695bc8ac1be7c89f7d0f9f6b1a1eb
Kevin Cooney <kcooney@users.noreply.github.com>
2025-09-23 20:24:46 -0700

    Create a record class for the build constants (#59)
    Motivation:
    - Makes the core code easier to understand (by centralizing the fields and
      reflection code)
    - Provides a path that could allow us to migrate our use of gversion to
      generate a properties file instead of a Java class (which would be much
      allow simpler extraction code)
    
    Also update BuildConstantsPublisher JavaDoc to reference the WPILib docs
    (so it doesn't get stale).

--- lib/src/main/java/com/team2813/lib2813/util/BuildConstantsPublisher.java
+++ lib/src/main/java/com/team2813/lib2813/util/BuildConstantsPublisher.java
@@ -2,6 +2,8 @@ package com.team2813.lib2813.util;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import java.util.Optional;
+import java.util.function.Function;
 
 /**
  * Publishes build constants to NetworkTables.
@@ -22,60 +24,25 @@ import edu.wpi.first.networktables.NetworkTableInstance;
  *
  * <p>To instantiate a BuildConstantsPublisher, a build constants class, {@code BuildConstants}, is
  * needs to be generated for the robot library by enabling the `gversion` plugin in the gradle build
- * file.
- *
- * <pre>{@code
- * plugins {
- *   ...
- *   // Plugin needed for Git Build Info
- *   // (see https://docs.wpilib.org/en/stable/docs/software/advanced-gradlerio/deploy-git-data.html)
- *   id 'com.peterabeles.gversion' version '1.10'
- *   ...
- * }
- * ...
- * // Generates a BuildConstants file.
- * // https://docs.wpilib.org/en/stable/docs/software/advanced-gradlerio/deploy-git-data.html
- * project.compileJava.dependsOn(createVersionFile)
- * def BUILD_CONSTANTS_AUTOGEN_PATH = 'build/generated/sources/build_constants/'
- * gversion {
- *     // Build inside build/ (so that it will be ignored by git due to .gitignore)
- *     // and inside build/generated/ (so that it will be ignored by our Spotless
- *     // rules).
- *     srcDir       = BUILD_CONSTANTS_AUTOGEN_PATH
- *     classPackage = 'com.team2813'
- *     className    = 'BuildConstants'
- *     dateFormat   = 'yyyy-MM-dd HH:mm:ss z'
- *     timeZone     = 'America/Los_Angeles' // Use preferred time zone
- *     indent       = '  '
- * }
- * sourceSets.main.java.srcDirs += BUILD_CONSTANTS_AUTOGEN_PATH
- * ...
- * }</pre>
+ * file. Instructions can be found <a
+ * href="https://docs.wpilib.org/en/stable/docs/software/advanced-gradlerio/deploy-git-data.html">in
+ * the WPILib documentation</a>.
  *
  * <p>With the BuildConstants generation enabled, the publisher is initialized and used like this:
  *
  * <pre>{@code
- * BuildConstantsPublisher buildConstantsPublisher(com.team2813.BuildConstants.class);
+ * BuildConstantsPublisher buildConstantsPublisher(frc.robot.BuildConstants.class);
  * // Publish the build constants to "/Metadata" on the NetworkTables
  * buildConstantsPublisher.publish(NetworkTableInstance.getDefault());
  * // Log the build constants in the robot console as well.
  * buildConstantsPublisher.log();
  * }</pre>
  */
-public class BuildConstantsPublisher {
+public final class BuildConstantsPublisher {
   /** The name of the NetworkTable under which the build constants are published. */
   public static final String METADATA_TABLE_NAME = "Metadata";
 
-  private String m_mavenName;
-  // Don't resolve BuildConstants.MAVEN_GROUP because it is always empty
-  private int m_gitRevision;
-  // Don't resolve BuildConstants.VERSION because it is always "unspecified".
-  private String m_gitSha;
-  private String m_gitDate;
-  private String m_gitBranch;
-  private String m_buildDate;
-  private long m_buildUnixTime;
-  private int m_dirty;
+  private final Optional<BuildConstantsRecord> constants;
 
   /**
    * Constructs a BuildConstantsPublisher.
@@ -87,20 +54,12 @@ public class BuildConstantsPublisher {
    *     constants. See class description for instructions on how to generate the class.
    */
   public BuildConstantsPublisher(Class<?> buildConstantsClass) {
-    try {
-      m_mavenName = (String) buildConstantsClass.getDeclaredField("MAVEN_NAME").get(null);
-      m_gitRevision = (int) buildConstantsClass.getDeclaredField("GIT_REVISION").get(null);
-      m_gitSha = (String) buildConstantsClass.getDeclaredField("GIT_SHA").get(null);
-      m_gitDate = (String) buildConstantsClass.getDeclaredField("GIT_DATE").get(null);
-      m_gitBranch = (String) buildConstantsClass.getDeclaredField("GIT_BRANCH").get(null);
-      m_buildDate = (String) buildConstantsClass.getDeclaredField("BUILD_DATE").get(null);
-      m_buildUnixTime = (long) buildConstantsClass.getDeclaredField("BUILD_UNIX_TIME").get(null);
-      m_dirty = (int) buildConstantsClass.getDeclaredField("DIRTY").get(null);
-    } catch (NoSuchFieldException | IllegalAccessException e) {
-      // TODO(vdikov): Add a proper error logging here so that developers can catch it when it
-      // happens
-      e.printStackTrace();
-    }
+    constants = BuildConstantsRecord.fromGeneratedClass(buildConstantsClass);
+  }
+
+  /** Gets the build constants extracted from the publisher. */
+  public Optional<BuildConstants> buildConstants() {
+    return constants.map(Function.identity());
   }
 
   /**
@@ -112,26 +71,32 @@ public class BuildConstantsPublisher {
    *     constants are published.
    */
   public void publish(NetworkTableInstance ntInstance) {
-    NetworkTable table = ntInstance.getTable(METADATA_TABLE_NAME);
-    table.getStringTopic("MavenName").publish().set(m_mavenName);
-    table.getIntegerTopic("GitRevision").publish().set(m_gitRevision);
-    table.getStringTopic("GitSha").publish().set(m_gitSha);
-    table.getStringTopic("GitDate").publish().set(m_gitDate);
-    table.getStringTopic("GitBranch").publish().set(m_gitBranch);
-    table.getStringTopic("BuildDate").publish().set(m_buildDate);
-    table.getIntegerTopic("BuildUnixTime").publish().set(m_buildUnixTime);
-    table.getIntegerTopic("Dirty").publish().set(m_dirty);
+    constants.ifPresent(
+        values -> {
+          NetworkTable table = ntInstance.getTable(METADATA_TABLE_NAME);
+          table.getStringTopic("MavenName").publish().set(values.mavenName());
+          table.getIntegerTopic("GitRevision").publish().set(values.gitRevision());
+          table.getStringTopic("GitSha").publish().set(values.gitSha());
+          table.getStringTopic("GitDate").publish().set(values.gitSubmitTimeString());
+          table.getStringTopic("GitBranch").publish().set(values.gitBranch());
+          table.getStringTopic("BuildDate").publish().set(values.buildTimeString());
+          table.getIntegerTopic("BuildUnixTime").publish().set(values.buildTimeMillis());
+          table.getIntegerTopic("Dirty").publish().set(values.dirty());
+        });
   }
 
   /** Logs the build constants to the console. */
   public void log() {
-    System.out.println("MavenName:     " + m_mavenName);
-    System.out.println("GitRevision:   " + m_gitRevision);
-    System.out.println("GitSha:        " + m_gitSha);
-    System.out.println("GitDate:       " + m_gitDate);
-    System.out.println("GitBranch:     " + m_gitBranch);
-    System.out.println("BuildDate:     " + m_buildDate);
-    System.out.println("BuildUnixTime: " + m_buildUnixTime);
-    System.out.println("Dirty:         " + m_dirty);
+    constants.ifPresent(
+        values -> {
+          System.out.println("MavenName:     " + values.mavenName());
+          System.out.println("GitRevision:   " + values.gitRevision());
+          System.out.println("GitSha:        " + values.gitSha());
+          System.out.println("GitDate:       " + values.gitSubmitTimeString());
+          System.out.println("GitBranch:     " + values.gitBranch());
+          System.out.println("BuildDate:     " + values.buildTimeString());
+          System.out.println("BuildUnixTime: " + values.buildTimeMillis());
+          System.out.println("Dirty:         " + values.dirty());
+        });
   }
 }
