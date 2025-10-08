635f00e1f7e164141d093a85c255e1ea89cd0083
Kevin Cooney <kcooney@gmail.com>
2025-10-07 20:52:04 -0700

    Remove unused constants, methods and classes

--- vision/src/main/java/com/team2813/lib2813/vision/CameraConstants.java
+++
@@ -1,9 +0,0 @@
-package com.team2813.lib2813.vision;
-
-class CameraConstants {
-  public static final String LIMELIGHT_CAMERA_NAME = "limelight";
-
-  private CameraConstants() {
-    throw new AssertionError("Not instantiable");
-  }
-}
--- vision/src/main/java/com/team2813/lib2813/vision/VisionNetworkTables.java
+++ vision/src/main/java/com/team2813/lib2813/vision/VisionNetworkTables.java
@@ -1,7 +1,5 @@
 package com.team2813.lib2813.vision;
 
-import static com.team2813.lib2813.vision.CameraConstants.LIMELIGHT_CAMERA_NAME;
-
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import org.photonvision.PhotonCamera;
@@ -10,15 +8,6 @@ import org.photonvision.PhotonCamera;
  * Contains methods and constants for publishing data from robot vision systems to network tables.
  */
 final class VisionNetworkTables {
-  /** Topic name to use when publishing whether a camera has current data. */
-  public static final String HAS_DATA_TOPIC = "hasData";
-
-  /** Topic name to use when publishing positions of detected AprilTags as a Pose3d array. */
-  public static final String VISIBLE_APRIL_TAG_POSES_TOPIC = "visibleAprilTagPoses";
-
-  /** Topic name to use when publishing the Pose3d position of a camera. */
-  static final String CAMERA_POSE_TOPIC = "cameraPose";
-
   /** Topic name to use when publishing the estimated robot position as a Pose2d value */
   static final String POSE_ESTIMATE_TOPIC = "poseEstimate";
 
@@ -28,33 +17,13 @@ final class VisionNetworkTables {
   private static final String TABLE_NAME = "Vision";
 
   /**
-   * Gets the network table for the camera with the given name
-   *
-   * <p>The key of the network table will be `Vision/[cameraName]`.
-   *
-   * @param ntInstance network table instance to publish to.
-   * @param cameraName name of the camera.
-   */
-  public static NetworkTable getTableForCamera(NetworkTableInstance ntInstance, String cameraName) {
-    return ntInstance.getTable(TABLE_NAME).getSubTable(cameraName);
-  }
-
-  /**
-   * Gets the network table for the provided photon vision camera.
+   * Gets the network table for the provided photon vision camera to use for publishing data.
    *
    * <p>The key of the network table will be `Vision/[cameraName]`.
    */
   public static NetworkTable getTableForCamera(PhotonCamera camera) {
-    return getTableForCamera(camera.getCameraTable().getInstance(), camera.getName());
-  }
-
-  /**
-   * Gets the network table for the limelight camera.
-   *
-   * @param ntInstance network table instance to publish to.
-   */
-  public static NetworkTable getTableForLimelight(NetworkTableInstance ntInstance) {
-    return getTableForCamera(ntInstance, LIMELIGHT_CAMERA_NAME);
+    NetworkTableInstance ntInstance = camera.getCameraTable().getInstance();
+    return ntInstance.getTable(TABLE_NAME).getSubTable(camera.getName());
   }
 
   private VisionNetworkTables() {
