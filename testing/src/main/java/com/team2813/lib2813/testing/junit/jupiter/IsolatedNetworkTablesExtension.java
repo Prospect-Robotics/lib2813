14600994535c2bfc4f09138d054b60865cca7eab
Kevin Cooney <kcooney@users.noreply.github.com>
2025-10-12 11:20:15 -0700

    Introduce IsolatedNetworkTablesExtension (#71)
    This is the JUnit5 version of NetworkTableResource.

---
+++ testing/src/main/java/com/team2813/lib2813/testing/junit/jupiter/IsolatedNetworkTablesExtension.java
@@ -0,0 +1,80 @@
+package com.team2813.lib2813.testing.junit.jupiter;
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Preferences;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.Extension;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+import org.junit.jupiter.api.extension.ExtensionContext.Store;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+/**
+ * JUnit Jupiter extension for providing an isolated NetworkTableInstance to tests.
+ *
+ * <p>Example use:
+ *
+ * <pre>{@code
+ * @ExtendWith(IsolatedNetworkTablesExtension.class)
+ * public final class IntakeTest {
+ *
+ *   @Test
+ *   public void intakeCoral(NetworkTableInstance ntInstance)  {
+ *     // Do something with ntInstance
+ *   }
+ * }
+ * }</pre>
+ */
+public final class IsolatedNetworkTablesExtension
+    implements Extension, AfterEachCallback, ParameterResolver {
+  private static final StoreKey<NetworkTableInstance> NETWORK_TABLE_INSTANCE_KEY =
+      StoreKey.of(NetworkTableInstance.class);
+
+  @Override
+  public void afterEach(ExtensionContext context) {
+    // If this extension created a temporary NetworkTableInstance, close it.
+    var ntInstance = NETWORK_TABLE_INSTANCE_KEY.get(getStore(context));
+    if (ntInstance != null) {
+      // Clear out the listener queue before destroying our temporary NetworkTableInstance.
+      //
+      // This works around a race condition in WPILib where a listener registered by Preferences can
+      // be called after the NetworkTableInstance was closed (see
+      // https://github.com/wpilibsuite/allwpilib/issues/8215).
+      if (!ntInstance.waitForListenerQueue(.1)) {
+        System.err.println(
+            "Timed out waiting for the NetworkTableInstance listener queue to empty (waited 100ms);"
+                + " JVM may crash");
+      }
+
+      Preferences.setNetworkTableInstance(NetworkTableInstance.getDefault());
+      ntInstance.close();
+    }
+  }
+
+  @Override
+  public boolean supportsParameter(
+      ParameterContext parameterContext, ExtensionContext extensionContext)
+      throws ParameterResolutionException {
+    return NetworkTableInstance.class.equals(parameterContext.getParameter().getType());
+  }
+
+  @Override
+  public NetworkTableInstance resolveParameter(
+      ParameterContext parameterContext, ExtensionContext extensionContext)
+      throws ParameterResolutionException {
+    Store store = getStore(extensionContext);
+    NetworkTableInstance ntInstance =
+        NETWORK_TABLE_INSTANCE_KEY.getOrComputeIfAbsent(store, NetworkTableInstance::create);
+
+    ntInstance.startLocal();
+    Preferences.setNetworkTableInstance(ntInstance);
+    return ntInstance;
+  }
+
+  /** Gets the {@link Store} for this extension. */
+  private Store getStore(ExtensionContext context) {
+    return context.getStore(Namespace.create(getClass(), context.getRequiredTestMethod()));
+  }
+}
