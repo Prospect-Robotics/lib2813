e9420c1149017a9724fa33fcfee83732b692d6c7
Kevin Cooney <kcooney@gmail.com>
2026-02-01 15:35:29 -0800

    Update IsolatedPreferences to restore previous nt instance for Preferences

--- lib/src/test/java/com/team2813/lib2813/preferences/IsolatedPreferences.java
+++ lib/src/test/java/com/team2813/lib2813/preferences/IsolatedPreferences.java
@@ -26,6 +26,7 @@ import org.junit.rules.ExternalResource;
  */
 final class IsolatedPreferences extends ExternalResource {
   private NetworkTableInstance tempInstance;
+  private NetworkTableInstance prevInstance;
 
   /** Gets the {@link NetworkTable} that contains the preference values. */
   public NetworkTable getPreferencesTable() {
@@ -34,7 +35,7 @@ final class IsolatedPreferences extends ExternalResource {
 
   @Override
   protected void before() {
-    NetworkTableInstance.getDefault();
+    prevInstance = Preferences.getNetworkTable().getInstance();
     tempInstance = NetworkTableInstance.create();
     tempInstance.startLocal();
     Preferences.setNetworkTableInstance(tempInstance);
@@ -42,7 +43,7 @@ final class IsolatedPreferences extends ExternalResource {
 
   @Override
   protected void after() {
-    Preferences.setNetworkTableInstance(NetworkTableInstance.getDefault());
+    Preferences.setNetworkTableInstance(prevInstance);
     if (!tempInstance.waitForListenerQueue(.6)) {
       System.err.println(
           "Timed out waiting for the NetworkTableInstance listener queue to empty (waited 600ms);"
