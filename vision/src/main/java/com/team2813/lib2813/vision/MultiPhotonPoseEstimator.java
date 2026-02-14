e75908e31e9b8c0c90f6274e98784ab53bb71f7a
Kevin Cooney <kcooney@gmail.com>
2026-02-14 13:11:38 -0800

    Add withMultiTagFallbackStrategy()

--- vision/src/main/java/com/team2813/lib2813/vision/MultiPhotonPoseEstimator.java
+++ vision/src/main/java/com/team2813/lib2813/vision/MultiPhotonPoseEstimator.java
@@ -67,7 +67,9 @@ import org.photonvision.simulation.VisionSystemSim;
 public class MultiPhotonPoseEstimator<C extends Camera> implements AutoCloseable {
   private final List<PhotonCameraWrapper<C>> cameraWrappers;
   private final Predicate<EstimatedRobotPose> isPoseValid;
+  private final PhotonPoseEstimator.PoseStrategy fallbackStrategy;
   private PhotonPoseEstimator.PoseStrategy poseEstimatorStrategy;
+  private boolean needHeadingData;
 
   /** A builder for {@code MultiPhotonPoseEstimator}. */
   public static final class Builder<C extends Camera> {
@@ -75,6 +77,8 @@ public class MultiPhotonPoseEstimator<C extends Camera> implements AutoCloseable
     private final AprilTagFieldLayout aprilTagFieldLayout;
     private final NetworkTableInstance ntInstance;
     private final PhotonPoseEstimator.PoseStrategy poseEstimatorStrategy;
+    private PhotonPoseEstimator.PoseStrategy fallbackStrategy =
+        PhotonPoseEstimator.PoseStrategy.LOWEST_AMBIGUITY;
     private Predicate<EstimatedRobotPose> isPoseValid = pose -> true;
 
     Builder(
@@ -113,6 +117,26 @@ public class MultiPhotonPoseEstimator<C extends Camera> implements AutoCloseable
      */
     public Builder<C> withPoseFilter(Predicate<EstimatedRobotPose> isPoseValid) {
       this.isPoseValid = isPoseValid;
+      return this;
+    }
+
+    /**
+     * Set the Position Estimation Strategy used in multi-tag mode when only one tag can be seen.
+     * Must NOT be {@link PhotonPoseEstimator.PoseStrategy#MULTI_TAG_PNP_ON_COPROCESSOR} or {@link
+     * PhotonPoseEstimator.PoseStrategy#MULTI_TAG_PNP_ON_RIO}.
+     *
+     * <p>If this is not called, {@link PhotonPoseEstimator.PoseStrategy#LOWEST_AMBIGUITY} will be
+     * used.
+     *
+     * @param strategy the strategy to set
+     * @return Builder instance.
+     * @since 2.1.0
+     */
+    public Builder<C> withMultiTagFallbackStrategy(PhotonPoseEstimator.PoseStrategy strategy) {
+      if (isMultiTagStrategy(strategy)) {
+        throw new IllegalArgumentException("Fallback strategy cannot be " + strategy);
+      }
+      fallbackStrategy = strategy;
       return this;
     }
 
@@ -251,11 +275,13 @@ public class MultiPhotonPoseEstimator<C extends Camera> implements AutoCloseable
   /** Creates an instance using values from a {@code Builder}. */
   private MultiPhotonPoseEstimator(Builder<C> builder) {
     poseEstimatorStrategy = builder.poseEstimatorStrategy;
+    fallbackStrategy = builder.fallbackStrategy;
     isPoseValid = poseIsInField(builder.aprilTagFieldLayout).and(builder.isPoseValid);
     cameraWrappers =
         builder.cameras.values().stream()
             .map(camera -> createCameraWrapper(builder, camera))
             .collect(toCollection(ArrayList::new));
+    needHeadingData = calculateNeedHeadingData();
   }
 
   /**
@@ -270,6 +296,7 @@ public class MultiPhotonPoseEstimator<C extends Camera> implements AutoCloseable
     PhotonPoseEstimator estimator =
         new PhotonPoseEstimator(
             builder.aprilTagFieldLayout, builder.poseEstimatorStrategy, camera.robotToCamera());
+    estimator.setMultiTagFallbackStrategy(builder.fallbackStrategy);
 
     // Create NetworkTables publishers for 1) the position of the camera relative to the robot and
     // 2) the estimated position provided by the camera.
@@ -302,6 +329,7 @@ public class MultiPhotonPoseEstimator<C extends Camera> implements AutoCloseable
     if (!poseStrategy.equals(poseEstimatorStrategy)) {
       cameraWrappers.forEach(wrapper -> wrapper.estimator.setPrimaryStrategy(poseStrategy));
       poseEstimatorStrategy = poseStrategy;
+      needHeadingData = calculateNeedHeadingData();
     }
   }
 
@@ -311,10 +339,7 @@ public class MultiPhotonPoseEstimator<C extends Camera> implements AutoCloseable
    * @return {@code true} if the pose strategy is documented to require addHeadingData().
    */
   public boolean poseStrategyRequiresHeadingData() {
-    return switch (poseEstimatorStrategy) {
-      case PNP_DISTANCE_TRIG_SOLVE, CONSTRAINED_SOLVEPNP -> true;
-      default -> false;
-    };
+    return needHeadingData;
   }
 
   /**
@@ -429,6 +454,29 @@ public class MultiPhotonPoseEstimator<C extends Camera> implements AutoCloseable
   public void close() {
     cameraWrappers.forEach(PhotonCameraWrapper::close);
     cameraWrappers.clear();
+  }
+
+  private static boolean poseStrategyRequiresHeadingData(
+      PhotonPoseEstimator.PoseStrategy strategy) {
+    return switch (strategy) {
+      case PNP_DISTANCE_TRIG_SOLVE, CONSTRAINED_SOLVEPNP -> true;
+      default -> false;
+    };
+  }
+
+  private static boolean isMultiTagStrategy(PhotonPoseEstimator.PoseStrategy strategy) {
+    return switch (strategy) {
+      case MULTI_TAG_PNP_ON_COPROCESSOR, MULTI_TAG_PNP_ON_RIO -> true;
+      default -> false;
+    };
+  }
+
+  private boolean calculateNeedHeadingData() {
+    if (poseStrategyRequiresHeadingData(poseEstimatorStrategy)) {
+      return true;
+    }
+    return isMultiTagStrategy(poseEstimatorStrategy)
+        && poseStrategyRequiresHeadingData(fallbackStrategy);
   }
 
   /** Creates a predicate that determines if a pose is inside the field. */
