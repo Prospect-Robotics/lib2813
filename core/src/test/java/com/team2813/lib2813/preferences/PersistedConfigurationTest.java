7a5035fb0890ef6b0746037b4635081762a59726
Kevin Cooney <kcooney@gmail.com>
2026-02-14 08:15:00 -0800

    Annotate PersistedConfigurationTest with @Execution(SAME_THREAD)
    I noticed the WPILib Preferences test uses this annotation, so trying
    it out to see if it reduces the crashes in the test runs.

--- core/src/test/java/com/team2813/lib2813/preferences/PersistedConfigurationTest.java
+++ core/src/test/java/com/team2813/lib2813/preferences/PersistedConfigurationTest.java
@@ -42,10 +42,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedClass;
 import org.junit.jupiter.params.provider.ValueSource;
 
 /** Tests for {@link PersistedConfiguration}. */
+@Execution(ExecutionMode.SAME_THREAD) // Test updates static state
 @ProvideUniqueNetworkTableInstance(replacePreferencesNetworkTable = true)
 public final class PersistedConfigurationTest {
   private static final double EPSILON = 0.001;
